de21a1173ba2671502d255c38aa026c5cb64ffc2
SudipRout <sudip.rout007@gmail.com>
2022-10-11 11:00:31 +0530

    initial commit

---
+++ AverageFunc.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class AverageFunc {
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the three numbers: ");
+        double a = sc.nextInt();
+        double b = sc.nextInt();
+        double c = sc.nextInt();
+
+        printAverage(a, b, c);
+    }
+    public static void printAverage(double a, double b, double c) {
+        double avg = (a+b+c)/3;
+        System.out.println("The average of the three numbers are: "+avg);
+    }
+}
